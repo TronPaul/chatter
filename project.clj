b832e806ad4e6e94ffefa1c900199c545ac474d4
Mark McGuire <mark.b.mcg@gmail.com>
2015-02-10 23:54:18 -0600

    start app

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject chatter "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [markov-text "0.0.2"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot chatter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chatter/core.clj
+++ src/chatter/core.clj
@@ -1,7 +1,35 @@
 (ns chatter.core
+  (:require [markov-text.core :as mkt]
+            [clojurewerkz.neocons.rest :as nr]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+(def ngram-size 3)
+
+(defn connect
+  [uri]
+  (nr/connect uri))
+
+(defn use-training-file
+  "Train the bot with a line separated training file"
+  [file conn]
+  (with-open [is (io/reader file)]
+    (doseq [line (line-seq is)]
+      (mkt/add-line line conn ngram-size))))
+
+(defn get-line
+  [conn]
+  (mkt/build-line conn))
+
+(def cli-opts
+  [["-c" "--connect" "neo4j REST URL"
+    :default "http://localhost:7474/db/data/"]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments]} (parse-opts args cli-opts)
+        conn (connect (:connect options))]
+    (use-training-file (first arguments) conn)
+    (println (get-line conn))))
