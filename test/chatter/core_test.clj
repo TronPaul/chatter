e48ad4bc7c9ef2e7281fb4e39965917da4b2379c
Mark McGuire <mark.b.mcg@gmail.com>
2015-02-14 17:32:49 -0600

    add filter tests

--- test/chatter/core_test.clj
+++ test/chatter/core_test.clj
@@ -1,7 +1,14 @@
 (ns chatter.core-test
   (:require [clojure.test :refer :all]
-            [chatter.core :refer :all]))
+            [chatter.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest own-tweets-irrelevant-test
+  (testing "Ensure the bot's tweets are irrelevant"
+    (is (#'core/irrelevant-mention? ::name {:user {:screen_name ::name}
+                                       :text "stuff"}))))
+
+(deftest retweets-irrelevant-test
+  (testing "Ensure retweets of the bot's tweets are irrelevant"
+    (is (#'core/irrelevant-mention? ::name {:user {:screen_name ::not_name}
+                                       :retweeted_status {:user {:screen_name ::name} :text "@somebody blah"}
+                                       :text "@someoneelse blah"}))))
